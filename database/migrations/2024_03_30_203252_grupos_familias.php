dca45612e95b7a806fdd06fa432771f396114652
Héctor Guerrero <hectordavid1995@hotmail.com>
2024-03-30 22:54:15 -0600

    Se agregan migraciones de db
    -familia producto
    -grupo familias
    -grupos requisitos

---
+++ database/migrations/2024_03_30_203252_grupos_familias.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grupos_productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_familia_producto');
+            $table->foreign('id_familia_producto')->references('id')->on('familia_producto');
+            $table->string('grupo',200)->nullable();
+            $table->string('descripcion',200)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('grupos_productos');
+    }
+};
