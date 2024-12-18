21bcecb7102187e42eee186b0f95d4957cb7be70
Henry Orellana <dev@volale.com>
2024-12-18 00:22:32 -0600

    mas cambios en pantallas

---
+++ app/Livewire/UploadDocumentReq.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Attributes\On;
+use Livewire\WithPagination;
+use Livewire\Component;
+use Session;
+use DB;
+
+class UploadDocumentReq extends Component
+{
+
+    use WithPagination;
+
+    public $Pagination = 10;
+    public $searchInput;
+
+    public function paginationView()
+    {
+        return 'vendor.livewire.bootstrap';
+    }
+
+    public function mount()
+    {
+
+    }
+
+
+    public function render()
+    {
+        return view('livewire.requisitos.upload-document-req')
+            ->extends('layouts.master')
+            ->section('content');
+    }
+
+    public function create()
+    {
+        try {
+            //este metodo lo que hace es inicailizar las transacciones en la base de datos
+            DB::beginTransaction();
+
+            //Aqui se escribe el codigo que se desea hacer en la transaccion
+
+            //este metodo lo que hace es guardar los cambios en la base de datos
+            DB::commit();
+
+        } catch (\Throwable $e) {
+            //este metodo lo que hace es deshacer los cambios en la base de datos
+            DB::rollback();
+
+            //este metodo lo que hace es mostrar el error en la consola
+            dd($e->getMessage());
+        }
+    }
+
+
+    public function update()
+    {
+        try {
+            //este metodo lo que hace es inicailizar las transacciones en la base de datos
+            DB::beginTransaction();
+
+            //Aqui se escribe el codigo que se desea hacer en la transaccion
+
+            //este metodo lo que hace es guardar los cambios en la base de datos
+            DB::commit();
+
+        } catch (\Throwable $e) {
+            //este metodo lo que hace es deshacer los cambios en la base de datos
+            DB::rollback();
+
+            //este metodo lo que hace es mostrar el error en la consola
+            dd($e->getMessage());
+        }
+    }
+
+
+    public function deletexid()
+    {
+        try {
+            //este metodo lo que hace es inicailizar las transacciones en la base de datos
+            DB::beginTransaction();
+
+            //Aqui se escribe el codigo que se desea hacer en la transaccion
+
+            //este metodo lo que hace es guardar los cambios en la base de datos
+            DB::commit();
+
+        } catch (\Throwable $e) {
+            //este metodo lo que hace es deshacer los cambios en la base de datos
+            DB::rollback();
+
+            //este metodo lo que hace es mostrar el error en la consola
+            dd($e->getMessage());
+        }
+    }
+
+
+    public function resetUI()
+    {
+
+
+    }
+
+}
